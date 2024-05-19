b50de6f3396d67b4baaa9d991f68d34bc0c44948
lindritdev <134339274+lindritdev@users.noreply.github.com>
2024-05-19 16:46:41 +0200

    Update guimodel.ts

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Lindrits Friend Tracker (FS2023)",
+            "title": "Arbers Friend Tracker (FS2023)",
             "formList": [
                 {
                     "id": "FriendForm",
